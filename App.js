32659f8d8ab943827df37ceae20c6aeea40da324
schiughi <schiughi@gmail.com>
2018-07-17 00:47:20 +0900

    fix component

--- App.js
+++ App.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { Constants } from 'expo';
-
+import { ButtonGroup } from 'react-native-elements'; 
 // You can import from local files
-import Number from './components/Number';
+import NumberCard from './components/NumberCard';
 
 // or any pure javascript modules available in npm
-import { Card, Button } from 'react-native-elements'; // Version can be specified in package.json
+// Version can be specified in package.json
 
 export default class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { num: "1" };
+    super(props)
+    this.buttons = ["0", "1","2", "3", "5", "8", "13", "21", "?"]
+
+    this.state = {
+      selectedIndex: 0
+    }
   }
-  onClick = (num) => {
-    this.setState({
-      num
-    })
+
+  onChange = (selectedIndex) => {
+    this.setState({ selectedIndex })
+  }
+
+  get num() {
+    return this.buttons[this.state.selectedIndex]
   }
 
   render() {
+    const { selectedIndex } = this.state
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>
           Planning Poker
         </Text>
-        <Card title="My estimation is ...">
-          <Number num={this.state.num} />
-        </Card>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <Button title="1" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("1")} />
-          <Button title="3" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("3")} />
-          <Button title="5" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("5")} />
-          <Button title="8" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("8")} />
-        </View>
-
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <Button title="13" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("13")} />
-          <Button title="21" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("21")} />
-          <Button title="?" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("?")} />
-          <Button title="0" containerViewStyle={{ width: '12%', margin: 5 }} onPress={() => this.onChange("0")} />
-        </View>
+        <NumberCard num={this.num} />
+        <ButtonGroup
+              selectedIndex={selectedIndex}
+              onPress={this.onChange}
+              buttons={this.buttons}
+              containerStyle={{height: 100}}
+        />
       </View>
     );
   }
--- components/NumberCard.js
+++ components/NumberCard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
+import { Card } from 'react-native-elements';
 
-export default class AssetExample extends Component {
+export default class NumberCard extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          {this.props.num}
-        </Text>
-      </View>
+      <Card title="My estimation is ..." on>
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>
+            {this.props.num}
+          </Text>
+        </View>
+      </Card>
     );
   }
 }
@@ -19,16 +22,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   paragraph: {
-    margin: 24,
+    width: '100%',
     marginTop: 0,
-    fontSize: 120,
+    fontSize: 200,
     fontWeight: 'bold',
     textAlign: 'center',
     color: '#34495e',
   },
-  logo: {
-    backgroundColor: "#056ecf",
-    height: 128,
-    width: 128,
-  }
 });
